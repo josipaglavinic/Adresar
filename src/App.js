bb6934eac4c740385910710e8ae3b6502ea9f40a
josipaglavinic <75853323+josipaglavinic@users.noreply.github.com>
2021-11-29 09:40:38 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
+import "./app.css";
 import React from "react";
-import { Route } from "react-router-dom";
-import { ToastContainer } from "react-toastify";
-import AddContact from "./components/AddContact/AddContact";
-import EditContact from "./components/EditContact/EditContact";
-import Home from "./components/Home/Home";
-import Login from "./components/Login/Login";
-import Navbar from "./components/Navbar/Navbar";
-import "./styles.css";
+import ContactsList from "./components/Contacts/ContactsList";
+import ContactContextProvider from "./context/ContactContext";
+import Header from "./components/Header/Header";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Favourites from "./components/Contacts/Favourites/Favourites";
+import EditContact from "./components/Contacts/EditContact";
+import AddContactForm from "./components/Contacts/AddContactForm";
 
-const App = () => {
+function App() {
   return (
     <div className="App">
-      <ToastContainer />
-      <Navbar />
-      <Route exact path="/" component={() => <Login />} />
-      <Route exact path="/contacts" component={() => <Home />} />
-      <Route exact path="/contacts/add" component={() => <AddContact />} />
-      <Route exact path="/contacts/:id" component={() => <EditContact />} />
+      <ContactContextProvider>
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<ContactsList />} />
+          <Route path="/contacts/add" element={<AddContactForm />} />
+          <Route path="/favourites" element={<Favourites />} />
+          <Route path="/contacts/add" element={<EditContact />} />
+        </Routes>
+      </ContactContextProvider>
     </div>
   );
-};
+}
+
 export default App;
